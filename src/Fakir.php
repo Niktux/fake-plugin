717959c9d5c0f0e7b3f9e3d6e42f0c9171779fa6
Niktux <nico.ln@gmail.com>
2017-02-19 23:07:08 +0100

    fake it !

--- src/Fakir.php
+++ src/Fakir.php
@@ -3,8 +3,30 @@
 namespace Onyx\Fake;
 
 use Onyx\Plugin;
+use Puzzle\Configuration;
+use Onyx\Plugins\AbstractPlugin;
+use Gaufrette\Adapter\Local;
+use Gaufrette\Filesystem;
+use Puzzle\Configuration\Yaml;
+use Onyx\DeleteMe\Pony;
 
-class Fakir implements Plugin
+class Fakir extends AbstractPlugin implements Plugin
 {
+    public function getName(): string
+    {
+        return "Onyx/Fakir";
+    }
 
+    public function getConfiguration(): ?Configuration
+    {
+        return new Yaml(
+            new Filesystem(
+                new Local(__DIR__ . '/../' . 'config/')
+        ));
+    }
+
+    public function getViewDirectories(): iterable
+    {
+        return [ __DIR__ . '/../' . 'views/'];
+    }
 }
